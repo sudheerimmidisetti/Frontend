05177abb82a86b452c92438d8386bf446a84777e
sudheerimmidisetti <sudheerimmidisetti@gmail.com>
2026-01-02 10:15:34 +0530

    Added JavaScript functionality files

---
+++ Js/demo.js
@@ -0,0 +1,23 @@
+// let num = prompt("Enter a number:");
+// num = Number(num);
+// console.log("You entered:", num);
+
+// function fun() {
+//     console.log("Hai");
+// }
+
+// window.onload = function () {
+//     alert("Page has fully loaded!");
+// };
+
+// window.onscroll = function () {
+//     console.log("Scrolled to X:", window.scrollX, "Scrolled to Y:", window.scrollY);
+// };
+
+// window.onresize = function () {
+//     console.log("Window size:", window.innerWidth, "x", window.innerHeight);
+// };
+
+// window.onblur = () => {
+//     console.log("Window lost focus");
+// };
